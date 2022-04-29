c80d525f6387022645aacb1ca5cd9dddcb607c22
Samir Cunha <samircunha98@gmail.com>
2022-04-28 21:58:48 -0300

    criação de components com CLI, criação de Pipes com CLI e testando funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //Permite trabalharmos com formulários, inputs, etc.
   ],
   providers: [
     CoursesService // com esta linha dizemos ao angular que esse service é um provedor de nossas classes. 
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,27 +1,76 @@
 import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
+// import { CoursesService } from './courses.service';
 
+//Working with pipes.
 @Component({
   selector: 'courses',
   template: `
-    <button class="btn btn-primary" [class.active]="isActive">save</button>
-    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Ok!</button>
-    <div (click)="onDivClicked()">
-    <button class="btn btn-secondary" (click)="onSave($event)">save</button>
-    </div>
-  `,
-})
-export class CoursesComponent {
-    isActive = false
-    onDivClicked() {
-        console.log("clicked")
-    }
+  {{course.title | uppercase | lowercase}} <br>
+  {{course.students | number}} <br>
+  {{course.rating | number:"2.1-2"}} <br>
+  {{course.price | currency:'BRL':true:'3.1-1'}} <br>
+  {{course.releaseDate | date:'longDate'}} <br>
+  `
+  })
 
-    onSave ($event: any) {
-        $event.stopPropagation(); // Não deixa que o click na div ocorra. 
-        console.log('Saved', $event)
-    }
+export class CoursesComponent {
+  course = {
+    title: 'The complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
 }
+
+
+
+
+//Stydies: Event Filtering, template variables, two-way binding
+// @Component({
+//   selector: 'courses',
+//   template: `
+//     <input #name (keyup.backspace)="getName(name.value)"/>
+//     <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+//   `,
+// })
+// export class CoursesComponent {
+//   email: string = 'samir@email.com';
+
+//   onKeyUp() { //maneira certa de pegar dados. 
+//     console.log(this.email);
+//   }
+
+//   getName(name: string) {
+//     console.log(name)
+//   }
+// }
+
+
+
+//Studies: Class binding, style binding, event binding
+// @Component({
+//   selector: 'courses',
+//   template: `
+//     <button class="btn btn-primary" [class.active]="isActive">save</button>
+//     <div>
+//     <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Ok!</button> 
+//     <div (click)="onDivClicked()">
+//     <button class="btn btn-secondary" (click)="onSave($event)">save</button> 
+//     </div>
+//   `,
+// })
+// export class CoursesComponent {
+//     isActive = false
+//     onDivClicked() {
+//         console.log("clicked")
+//     }
+
+//     onSave ($event: any) {
+//         $event.stopPropagation(); // Não deixa que o click na div ocorra. 
+//         console.log('Saved', $event)
+//     }
+// }
 
 
 
